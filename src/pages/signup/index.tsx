971de24f988c9f53c9a9eae9349b795118b15f4d
dayannealcantara <dayannealcantara14@gmail.com>
2024-06-03 12:18:13 -0300

    feat: developing login page

---
+++ src/pages/signup/index.tsx
@@ -0,0 +1,47 @@
+import  Head  from "next/head";
+import logoImg from '../../../public/logo.svg'
+import styles from '../../../styles/home.module.scss'
+import Image from "next/image";
+import Input from "../../components/Input";
+import Button from "../../components/Button";
+import Link from "next/link";
+import { FormEvent, useState } from "react";
+
+
+export default function SignUp() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const [loading, setLoading] = useState(false)
+
+  function handleSignUp (event : FormEvent){
+    if (name=== '' || email === '' || password === ''){
+      alert("Preencha todos os campos")
+      return;
+    }
+    setLoading(true)
+
+  }
+  
+
+  return (
+    <>
+      <Head>
+        <title>SujeitoPizza - Cadastro</title>
+      </Head>
+      <div className={styles.containerCenter}>
+        <Image src={logoImg} alt="logo da pizzaria" width={476} height={106}/>
+        <div className={styles.formContainer}>
+          <form onSubmit={handleSignUp} >
+          <Input type="text" placeholder="Nome da empresa" value={name} onChange={(e) => setName(e.target.value)}/>
+          <Input type="text" placeholder="Digite seu email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+          <Input  type="password" placeholder="Sua senha" value={password} onChange={(e) => setPassword(e.target.value)}/>
+          <Button type="submit" loading={loading}>Cadastrar</Button>
+          </form>          
+          <Link href="/" className={styles.text}> Já possuo uma conta? Faça login!</Link>
+        </div> 
+      </div>
+    </>
+  )
+}
