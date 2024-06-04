8b8db51b46e3a99b026844ad3d5d7ec026cb18b9
dayannealcantara <dayannealcantara14@gmail.com>
2024-06-04 11:31:11 -0300

    feat: staying logged in and grabbing user data

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,6 +1,6 @@
-import { createContext, ReactNode, useState } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
 import SignUp from '../pages/signup/index';
-import { destroyCookie, setCookie } from "nookies";
+import { destroyCookie, parseCookies, setCookie } from "nookies";
 import  Router from "next/router";
 import { api } from "../services/apiClient";
 import { toast } from "react-toastify";
@@ -48,6 +48,24 @@ type AuthProviderProps = {
 export function AuthProvider({children}: AuthProviderProps){
   const [user, setUser] = useState<UserProps>()
   const isAuthenticated = !!user
+
+  useEffect(() => {
+    const {'@nextauth.token' : token} = parseCookies()
+
+      if(token) {
+        api.get('/me').then(response => {
+          const {id, name, email} = response.data
+
+          setUser({
+            id, name, email
+          })
+        })
+        .catch(() => {
+          signOut()
+        })
+      }
+    }, [])
+
 
   async function signIn({email, password}: SignInProps){
     try {
