dd4b70127e20e39e7d0c3e7640eb0c5da4a4886d
dayannealcantara <dayannealcantara14@gmail.com>
2024-06-05 11:59:37 -0300

    feat: finishing dashboard page

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,23 +1,50 @@
-import  Head  from "next/head";
-import { canSSRAuth } from "../../utils/canSSRAuth";
-import { Header } from "../../components/Header/indes";
+import { canSSRAuth } from '../../utils/canSSRAuth'
+import Head from 'next/head';
+import styles from './styles.module.scss';
 
 
-export default function Dashboard() {
-  return (
+import { FiRefreshCcw } from 'react-icons/fi'
+import { Header } from '../../components/Header/indes';
+
+export default function Dashboard(){
+  return(
     <>
-      <Head>
-        <title>Painel - Sujeito pizzaria</title>
-      </Head>
-      <div>
-        <Header/>
-      </div>        
+    <Head>
+      <title>Painel - Sujeito Pizzaria</title>
+    </Head>
+    <div>
+      <Header/>
+    
+      <main className={styles.container}>
+
+        <div className={styles.containerHeader}>
+          <h1>Últimos pedidos</h1>
+          <button>
+            <FiRefreshCcw size={25} color="#3fffa3"/>
+          </button>
+        </div>
+
+        <article className={styles.listOreders}>
+
+          <section className={styles.orderItem}> 
+            <button>
+              <div className={styles.tag}></div>
+              <span>Mesa 30</span>
+            </button>
+          </section>                  
+
+        </article>
+
+      </main>
+
+    </div>
     </>
   )
 }
 
 export const getServerSideProps = canSSRAuth(async (ctx) => {
+
   return {
-    props:{}
+    props: {}
   }
 })
